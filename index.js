4afd21b350b2d75c919748dc87b11bb0848a15db
bharathrames <bharath91505@gmail.com>
2023-10-01 20:23:18 +0530

    cors

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT;
-
+app.use(cors({
+  // origin:"http://localhost:3000"
+  origin:"https://transactionsdashboard.netlify.app/"
+}))
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 
 const productSchema = new mongoose.Schema({
   id: Number,
@@ -24,22 +25,19 @@ const productSchema = new mongoose.Schema({
   dateOfSale: Date,
 });
 
-
 const Product = mongoose.model('Product', productSchema);
-
 
 app.get('/fetch-data', async (req, res) => {
   try {
-    
-    const response = await axios.get(
-      'https://s3.amazonaws.com/roxiler.com/product_transaction.json'
-    );
+    const fetchResponse = await fetch('https://s3.amazonaws.com/roxiler.com/product_transaction.json');
+    if (!fetchResponse.ok) {
+      throw new Error('Failed to fetch data');
+    }
 
-    
-    const products = response.data;
+    const products = await fetchResponse.json();
     await Product.insertMany(products);
 
-    res.json({ message: 'sucessfully imported data' });
+    res.json({ message: 'Successfully imported data' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error importing data' });
@@ -48,16 +46,13 @@ app.get('/fetch-data', async (req, res) => {
 
 app.get('/data', async (req, res) => {
   try {
-    
     const products = await Product.find();
-
     res.json(products);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error fetching data' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
